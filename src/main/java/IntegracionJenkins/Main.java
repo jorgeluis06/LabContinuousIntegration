c7486b1b45192b0fb50da6b77b4c677fff1e35f1
Your NameJorge <jorgeluis993@hotmail.com>
2021-07-23 00:26:02 -0500

    Actualizacion del contenido del repositorio

--- src/main/java/IntegracionJenkins/Main.java
+++ src/main/java/IntegracionJenkins/Main.java
@@ -16,8 +16,8 @@ public class Main {
         final int min_width = 2;
         
         //Dimension of the object
-        final int object_height = 12;
-        final int object_width = 10;
+        final int object_height=12;
+        final int object_width=10;
         
         relOp.goodHeight(object_height, min_height, max_height);
         
